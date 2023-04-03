f0324609d9d67360b7a74014ef8ac196499ed24d
Eugen <eugeniu.popa@isa.utm.md>
2023-04-03 11:55:47 +0300

    add week4 minimal

--- lab1/lib/printer.ex
+++ lab1/lib/printer.ex
@@ -30,7 +30,27 @@ defmodule Printer do
       end
     end)
     censored_tweet = Enum.join(censored_words, " ")
-    IO.puts("Received tweet: #{censored_tweet} \n")
+    emotion_values_url = "localhost:4000/emotion_values"
+    emotion_values = HTTPoison.get!(emotion_values_url).body
+    emotion_values_strings = String.split(emotion_values, ["\n", "\t"]) |> Enum.map(&String.replace(&1, "\r", ""))
+    emotion_values_pairs = Enum.chunk_every(emotion_values_strings, 2)
+    emotion_values_dict = emotion_values_pairs |> Enum.reduce(%{}, fn [word, score], acc -> Map.put(acc, word, score) end)
+    values = Enum.map(tweet_words, fn word ->
+      case Map.get(emotion_values_dict, word) do
+        nil -> 0
+        val -> String.to_integer(val)
+      end
+    end)
+    sum_of_values = Enum.reduce(values, 0, fn x, acc -> x + acc end)
+    sentiment_score = sum_of_values / length(values)
+    favorite_count = data["message"]["tweet"]["retweeted_status"]["favorite_count"] || 0
+    retweet_count = data["message"]["tweet"]["retweeted_status"]["retweet_count"] || 0
+    followers_count = data["message"]["tweet"]["user"]["followers_count"]
+    engagement_ratio =
+      if followers_count == 0,
+        do: 0,
+        else: (favorite_count + retweet_count) / followers_count
+    IO.puts("Received tweet: #{censored_tweet}\nSentiment score: #{sentiment_score}\nEngagement ratio: #{engagement_ratio}\n")
     min = 5
     max = 50
     lambda = Enum.sum(min..max) / Enum.count(min..max)
